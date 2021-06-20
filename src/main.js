cf8257a1c930b6eb02839a5b8dc1df628b2584d5
JorgeAlf09 <jorgelrv1996@hotmail.com>
2021-06-19 21:20:50 -0500

    Se agregó función al textbox para que sólo permita letras.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
+//Función para validar que no hayan números.
+function SoloLetras(e){
+    key = e.keyCode || e.which;
+    teclado = String.fromCharCode(key).toLowerCase();
+    letras = " abcdefghijklmnñopqrstuvwxyzáéíóú";
+    especiales = "8-37-38-46-164";//array
+    teclado_especial = false;
+
+    for(var i in especiales){
+        if(key == especiales [i]){
+            teclado_especial = true; break;
+        }
+    }
+
+    if(letras.indexOf(teclado) == -1 && !teclado_especial){
+        alert("El caracter ingresado no es válido, por favor ingresar un nombre válido omitiendo números.");
+    }
+
+}
+
+
 //Función de captura de datos para reflejar el nombre del jugador.
 function captura(){
     var nombrejug = document.getElementById("nombre").value;
