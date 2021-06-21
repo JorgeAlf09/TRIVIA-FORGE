02bcc51d4cd2da3ca96cb158ebc86534a1a506f6
JorgeAlf09 <jorgelrv1996@hotmail.com>
2021-06-20 22:44:52 -0500

    Se agregó función de cronómetro, pero aún no se creó el elemento para ello, ya que primero se necesitan los objetos para las trivias.

--- src/main.js
+++ src/main.js
@@ -101,3 +101,17 @@ function volverHome(){
     document.querySelector('#saludjug').innerText = "POR FAVOR INGRESA TU NOMBRE";
     document.getElementById("txtbox").value = "";
 }
+
+//--------------FUNCIONES DURANTE LAS TRIVIAS---------------------------//
+
+//Función del cronómetro.
+function cronom(){
+    var cuentaRegre = 20;
+    document.getElementById("cuenRegr").innerHTML = cuentaRegre;
+    if(cuentaRegre == 0){
+        alert("Su tiempo ha finalizado.");
+    }else{
+        cuentaRegre -= 1;
+        setTimeout(cuentaRegre, 1E3); //El 1E3 es igual a escribir 1000, estoy pidiendo que en 1000 milisegundos reduzca el tiempo.
+    }
+}
