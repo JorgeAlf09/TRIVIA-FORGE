d0e3edcc2bbd1928e66b8062c1d42b80d4113778
JorgeAlf09 <jorgelrv1996@hotmail.com>
2021-06-21 23:08:43 -0500

    Ya se agregó el panel para las trivias, se ocultó para que sólo se muestre al empezar el juego, y se agregó función de sólo lectura.

--- src/main.js
+++ src/main.js
@@ -108,6 +108,7 @@ function mostrarIniTriv(){
     document.getElementById("btnEmp2").style.display = 'none';
     document.getElementById("label3").style.display = 'none';
     document.getElementById("btnVolver").style.display = 'none';
+    document.getElementById("preguntas_trivia").style.display = 'block';
 }
 
 //--------------FUNCIONES DURANTE LAS TRIVIAS---------------------------//
@@ -122,4 +123,19 @@ function cronom(){
         cuentaRegre -= 1; // Quiere decir que va reduciendo de 1 en 1.
         setTimeout(cuentaRegre, 1E3); //El 1E3 es igual a escribir 1000, estoy pidiendo que en 1000 milisegundos reduzca el tiempo.
     }
+}
+
+
+//Función de lectura de textos de ruta local, no global, para poder plasmar las preguntas.
+function readText(ruta_local){
+    var texto = null;
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open("GET", ruta_local, false);
+    xmlhttp.send();
+
+    if(xmlhttp.status == 200){
+        texto = xmlhttp.responseText;
+    }
+    return texto;
 }
