3de8699f6d04a5ad290bd3e07fa2c333f03de3e2
JorgeAlf09 <jorgelrv1996@hotmail.com>
2021-06-22 14:02:40 -0500

    Se agregaron más funciones, pero aún no se conecta con el .js.

--- src/preguntas/preg_lucha/preguntas.js
+++ src/preguntas/preg_lucha/preguntas.js
@@ -1,5 +1,17 @@
 //--------------FUNCIONES DURANTE LAS TRIVIAS---------------------------//
 
+let base_preguntas = readText("base-preguntas-lucha.json");
+let interpretar_bp = JSON.parse(base_preguntas);
+let pregunta
+let posibles_respuestas
+let btn_respectivo = [ //Arreglo de botones.
+    select_id("resp1"),
+    select_id("resp2"),
+    select_id("resp3"),
+    select_id("resp4")
+]
+
+
 window.onload = function(){
     base_preguntas = readText("base-preguntas-lucha.json");
     interpretar_bp = JSON.parse(base_preguntas);
@@ -25,12 +37,61 @@ function select_id(id){
 
 //Función escoger pregunta.
 function escogerPregunta(n){
-    let base_preguntas = readText("base-preguntas-lucha.json");
-    let interpretar_bp = JSON.parse(base_preguntas);
     pregunta = interpretar_bp[n];
+
     select_id("label1").innerHTML = pregunta.categoria;
     select_id("label2").innerHTML = pregunta.pregunta;
-    select_id("foto").setAttribute("src",pregunta.foto)
+    select_id("foto").setAttribute("src",pregunta.imagen);
+    style("foto").objectFit = pregunta.objectFit;
+    select_id("resp1").innerHTML = respunta.respuesta;
+    select_id("resp2").innerHTML = respunta.incorrecta1;
+    select_id("resp3").innerHTML = respunta.incorrecta2;
+    select_id("resp4").innerHTML = respunta.incorrecta3;
+    if(pregunta.imagen){
+        style("foto").height = "200px";
+        style("foto").width = "100%";
+    }else{
+        style("foto").height = "0px";
+        style("foto").width = "0px";
+    }
+}
+
+//Función para que pueda escoger las preguntas de forma aleatoria en Música.
+function escogerPreguntaAleatoria(){
+    escogerPreguntaAleatoria(Math.floor(Math.random()*interpretar_bp.lenght))
+}
+
+
+//Función para desordenar respuestas.
+function desordenarRespuestas(pregunta){
+    let posibles_respuestas = [ //Se le cambió el nombre al arr(de array o arreglo) para que se entienda.
+        pregunta.respuesta,
+        pregunta.incorrecta1,
+        pregunta.incorrecta2,
+        pregunta.incorrecta3
+    ];
+    posibles_respuestas.sort(()=>Math.random()-0.5);
+
+    select_id("preg1").innerHTML = posibles_respuestas[0];
+    select_id("preg2").innerHTML = posibles_respuestas[1];
+    select_id("preg3").innerHTML = posibles_respuestas[2];
+    select_id("preg4").innerHTML = posibles_respuestas[3];
+}
+
+//Función para presionar botones.
+function presionar_boton(){
+    if(posibles_respuestas[i]==pregunta.respuesta){
+        btn_respectivo[i].style.background = "green";
+    }else{
+        btn_respectivo[i].style.background = "red";
+    }
+}
+
+//Función para reiniciar los botones a blanco.
+function reiniciarColores(){
+    for(const btn of btn_respectivo){
+        btn.style.background = "white";
+    }
 }
 
 //Función de lectura de textos de ruta local, no global, para poder plasmar las preguntas.
@@ -45,13 +106,4 @@ function readText(ruta_local){
         texto = xmlhttp.responseText;
     }
     return texto;
-}
-
-
-//Función para que pueda escoger las preguntas de forma aleatoria en Música.
-function preguntasMusica(){
-    let base_preguntas = readText("base-preguntas-lucha.json")
-    let refleja_bp = JSON.parse(base_preguntas)
-
-    preguntasMusica(Math.floor(Math.random()*refleja_bp.lenght))
 }
