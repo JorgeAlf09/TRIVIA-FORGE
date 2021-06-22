298e66ca5426a9b2e152ef8d1fe10b1c2eaf3c00
JorgeAlf09 <jorgelrv1996@hotmail.com>
2021-06-22 12:36:04 -0500

    Se agregaron fotos para música y lucha libre.

--- src/preguntas/preg_lucha/preguntas.js
+++ src/preguntas/preg_lucha/preguntas.js
@@ -1,5 +1,11 @@
 //--------------FUNCIONES DURANTE LAS TRIVIAS---------------------------//
 
+window.onload = function(){
+    base_preguntas = readText("base-preguntas-lucha.json");
+    interpretar_bp = JSON.parse(base_preguntas);
+    escogerPreguntaAleatoria();
+}
+
 //Función del cronómetro.
 function cronom(){
     var cuentaRegre = 20;
@@ -12,6 +18,20 @@ function cronom(){
     }
 }
 
+//Función para seleccionar id.
+function select_id(id){
+    return document.getElementById(id)
+}
+
+//Función escoger pregunta.
+function escogerPregunta(n){
+    let base_preguntas = readText("base-preguntas-lucha.json");
+    let interpretar_bp = JSON.parse(base_preguntas);
+    pregunta = interpretar_bp[n];
+    select_id("label1").innerHTML = pregunta.categoria;
+    select_id("label2").innerHTML = pregunta.pregunta;
+    select_id("foto").setAttribute("src",pregunta.foto)
+}
 
 //Función de lectura de textos de ruta local, no global, para poder plasmar las preguntas.
 function readText(ruta_local){
@@ -30,7 +50,7 @@ function readText(ruta_local){
 
 //Función para que pueda escoger las preguntas de forma aleatoria en Música.
 function preguntasMusica(){
-    let base_preguntas = readText("base-preguntas-musica.json")
+    let base_preguntas = readText("base-preguntas-lucha.json")
     let refleja_bp = JSON.parse(base_preguntas)
 
     preguntasMusica(Math.floor(Math.random()*refleja_bp.lenght))
