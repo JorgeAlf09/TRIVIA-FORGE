4550064014ec150e0ebd19a14c2969e5184e7dd7
JorgeAlf09 <jorgelrv1996@hotmail.com>
2021-06-23 00:49:59 -0500

    Se agregó nuenva función al cronómetro, pero falta hacer ajustes.

--- src/preguntas/preg_lucha/preg_luchas.js
+++ src/preguntas/preg_lucha/preg_luchas.js
@@ -1,16 +1,49 @@
 //--------------FUNCIONES DURANTE LAS TRIVIAS---------------------------//
-var cuentaRegre = 20;
+var cuentaRegre = 20; //PARA CRONÓMETRO
+var cantPreg = 1; //PARA CRONÓMETRO
 
 //Función del cronómetro.
 function cronom(){
     document.getElementById("countdown").innerHTML = cuentaRegre;
+        if(cuentaRegre == 0){
+            cuentaRegre = 20;
+            alert("Su tiempo ha finalizado. Continúa con la siguiente pregunta");
+            pregRecib();
+            reiniciarCronom();
+            detenerJuego();
+        }else{
+            cuentaRegre--; // Quiere decir que va reduciendo de 1 en 1.
+            setTimeout("cronom()", 1E3); //El 1E3 es igual a escribir 1000, estoy pidiendo que en 1000 milisegundos reduzca el tiempo.
+        }
 
-    if(cuentaRegre == 0){
-        alert("Su tiempo ha finalizado.");
+}
+
+//Función cantidad de preguntas recibidas.
+function pregRecib(){
+    document.getElementById("cant_de_preg").innerHTML = cantPreg + " de 4";
+
+    if(cantPreg == 4){
+        alert("Se terminó el juego");
     }else{
-        cuentaRegre--; // Quiere decir que va reduciendo de 1 en 1.
-        setTimeout("cronom()", 1E3); //El 1E3 es igual a escribir 1000, estoy pidiendo que en 1000 milisegundos reduzca el tiempo.
+        cantPreg++;
+    }
+}
+
+//Función reiniciar cronómetro.
+function reiniciarCronom(){
+    document.getElementById("countdown").innerHTML = cuentaRegre;
+    if(cronom()==0){
+        cuentaRegre = 20;
+    }
+}
+
+//Función de detener cuenta regresiva.
+function detenerJuego(){
+    document.getElementById("cant_de_preg").innerHTML = cantPreg + " de 4";
+    if(cantPreg==4){
+        document.getElementById("countdown").style.display = 'none';
     }
 }
 
 cronom();
+pregRecib();
