cb6ee657b1ff145d5395daa875638b1ab922022e
JorgeAlf09 <jorgelrv1996@hotmail.com>
2021-06-25 22:19:39 -0500

    Se corrige función del cronómetro y del contador de preguntas.

--- src/preguntas/preg_lucha/preg_luchas.js
+++ src/preguntas/preg_lucha/preg_luchas.js
@@ -10,7 +10,7 @@ function cronom(){
             alert("Su tiempo ha finalizado. Continúa con la siguiente pregunta");
             pregRecib();
             reiniciarCronom();
-            detenerJuego();
+            
         }else{
             cuentaRegre--; // Quiere decir que va reduciendo de 1 en 1.
             setTimeout("cronom()", 1E3); //El 1E3 es igual a escribir 1000, estoy pidiendo que en 1000 milisegundos reduzca el tiempo.
@@ -22,8 +22,10 @@ function cronom(){
 function pregRecib(){
     document.getElementById("cant_de_preg").innerHTML = cantPreg + " de 4";
 
-    if(cantPreg == 4){
+    if(cantPreg == 5){
         alert("Se terminó el juego");
+        document.getElementById("countdown").style.display = 'none';
+        document.getElementById("cant_de_preg").innerHTML = "4 de 4";
     }else{
         cantPreg++;
     }
@@ -39,9 +41,8 @@ function reiniciarCronom(){
 
 //Función de detener cuenta regresiva.
 function detenerJuego(){
-    document.getElementById("cant_de_preg").innerHTML = cantPreg + " de 4";
-    if(cantPreg==4){
-        document.getElementById("countdown").style.display = 'none';
+    if(cantPreg == 6){
+        
     }
 }
 
